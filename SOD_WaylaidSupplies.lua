d0d401847b33b56891c8e83116168e19bf922f7f
Nykk Romancer <nykk.romancer@gmail.com>
2024-03-14 13:40:59 -1000

    Initial commit

---
+++ SOD_WaylaidSupplies.lua
@@ -0,0 +1,746 @@
+-- Define the namespace if it doesn't exist
+NykkRomancer = NykkRomancer or {}
+
+-- Define Waylaid Supplies if another addon hasn't already brought them in
+NykkRomancer.SOD_WaylaidSupplies = NykkRomancer.SOD_WaylaidSupplies or {
+    [215396] = {
+        Name = "Waylaid Supplies: Elixirs of Greater Defense",
+        Level = 40,
+        Reputation = 1000,
+        Money = 120000,
+        Item = {
+            ID = 8951,
+            Quantity = 14,
+        },
+    },
+    [215397] = {
+        Name = "Waylaid Supplies: Massive Iron Axes",
+        Level = 40,
+        Reputation = 1000,
+        Money = 120000,
+        Item = {
+            ID = 3855,
+            Quantity = 2,
+        },
+    },
+    [215404] = {
+        Name = "Waylaid Supplies: Mithril Blunderbuss",
+        Level = 40,
+        Reputation = 1000,
+        Money = 120000,
+        Item = {
+            ID = 10508,
+            Quantity = 2,
+        },
+    },
+    [215409] = {
+        Name = "Waylaid Supplies: Turtle Scale Bracers",
+        Level = 40,
+        Reputation = 1000,
+        Money = 120000,
+        Item = {
+            ID = 8198,
+            Quantity = 2,
+        },
+    },
+    [215416] = {
+        Name = "Waylaid Supplies: White Bandit Masks",
+        Level = 40,
+        Reputation = 1000,
+        Money = 120000,
+        Item = {
+            ID = 10008,
+            Quantity = 3,
+        },
+    },
+    [215407] = {
+        Name = "Waylaid Supplies: Barbaric Shoulders",
+        Level = 35,
+        Reputation = 850,
+        Money = 50000,
+        Item = {
+            ID = 5964,
+            Quantity = 4,
+        },
+    },
+    [215401] = {
+        Name = "Waylaid Supplies: Compact Harvest Reaper Kits",
+        Level = 35,
+        Reputation = 850,
+        Money = 50000,
+        Item = {
+            ID = 4391,
+            Quantity = 2,
+        },
+    },
+    [215414] = {
+        Name = "Waylaid Supplies: Crimson Silk Pantaloons",
+        Level = 35,
+        Reputation = 850,
+        Money = 50000,
+        Item = {
+            ID = 7062,
+            Quantity = 4,
+        },
+    },
+    [215395] = {
+        Name = "Waylaid Supplies: Elixirs of Agility",
+        Level = 35,
+        Reputation = 850,
+        Money = 50000,
+        Item = {
+            ID = 8949,
+            Quantity = 6,
+        },
+    },
+    [215385] = {
+        Name = "Waylaid Supplies: Gold Bars",
+        Level = 35,
+        Reputation = 850,
+        Money = 50000,
+        Item = {
+            ID = 3577,
+            Quantity = 4,
+        },
+    },
+    [215393] = {
+        Name = "Waylaid Supplies: Greater Healing Potions",
+        Level = 35,
+        Reputation = 850,
+        Money = 50000,
+        Item = {
+            ID = 1710,
+            Quantity = 16,
+        },
+    },
+    [215399] = {
+        Name = "Waylaid Supplies: Heavy Mithril Gauntlets",
+        Level = 35,
+        Reputation = 850,
+        Money = 50000,
+        Item = {
+            ID = 7919,
+            Quantity = 3,
+        },
+    },
+    [215419] = {
+        Name = "Waylaid Supplies: Heavy Silk Bandages",
+        Level = 35,
+        Reputation = 700,
+        Money = 20000,
+        Item = {
+            ID = 6451,
+            Quantity = 10,
+        },
+    },
+    [215390] = {
+        Name = "Waylaid Supplies: Khadgar's Whisker",
+        Level = 35,
+        Reputation = 700,
+        Money = 20000,
+        Item = {
+            ID = 3358,
+            Quantity = 10,
+        },
+    },
+    [215386] = {
+        Name = "Waylaid Supplies: Mithril Bars",
+        Level = 35,
+        Reputation = 700,
+        Money = 20000,
+        Item = {
+            ID = 3860,
+            Quantity = 6,
+        },
+    },
+    [215392] = {
+        Name = "Waylaid Supplies: Purple Lotus",
+        Level = 35,
+        Reputation = 700,
+        Money = 20000,
+        Item = {
+            ID = 8831,
+            Quantity = 8,
+        },
+    },
+    [215415] = {
+        Name = "Waylaid Supplies: Rich Purple Silk Shirts",
+        Level = 35,
+        Reputation = 1000,
+        Money = 120000,
+        Item = {
+            ID = 4335,
+            Quantity = 5,
+        },
+    },
+    [215417] = {
+        Name = "Waylaid Supplies: Soothing Turtle Bisque",
+        Level = 35,
+        Reputation = 700,
+        Money = 20000,
+        Item = {
+            ID = 3729,
+            Quantity = 10,
+        },
+    },
+    [215418] = {
+        Name = "Waylaid Supplies: Spider Sausages",
+        Level = 35,
+        Reputation = 700,
+        Money = 20000,
+        Item = {
+            ID = 17222,
+            Quantity = 5,
+        },
+    },
+    [215388] = {
+        Name = "Waylaid Supplies: Thick Leather",
+        Level = 35,
+        Reputation = 700,
+        Money = 20000,
+        Item = {
+            ID = 4304,
+            Quantity = 10,
+        },
+    },
+    [215402] = {
+        Name = "Waylaid Supplies: Big Iron Bombs",
+        Level = 30,
+        Reputation = 850,
+        Money = 50000,
+        Item = {
+            ID = 4394,
+            Quantity = 8,
+        },
+    },
+    [215403] = {
+        Name = "Waylaid Supplies: Deadly Scopes",
+        Level = 30,
+        Reputation = 1000,
+        Money = 120000,
+        Item = {
+            ID = 10546,
+            Quantity = 2,
+        },
+    },
+    [215389] = {
+        Name = "Waylaid Supplies: Fadeleaf",
+        Level = 30,
+        Reputation = 700,
+        Money = 20000,
+        Item = {
+            ID = 2042,
+            Quantity = 16,
+        },
+    },
+    [215421] = {
+        Name = "Waylaid Supplies: Fire Oil",
+        Level = 30,
+        Reputation = 700,
+        Money = 20000,
+        Item = {
+            ID = 6371,
+            Quantity = 7,
+        },
+    },
+    [215413] = {
+        Name = "Waylaid Supplies: Formal White Shirts",
+        Level = 30,
+        Reputation = 700,
+        Money = 20000,
+        Item = {
+            ID = 4334,
+            Quantity = 3,
+        },
+    },
+    [215411] = {
+        Name = "Waylaid Supplies: Frost Leather Cloaks",
+        Level = 30,
+        Reputation = 850,
+        Money = 50000,
+        Item = {
+            ID = 7377,
+            Quantity = 2,
+        },
+    },
+    [215398] = {
+        Name = "Waylaid Supplies: Green Iron Bracers",
+        Level = 30,
+        Reputation = 850,
+        Money = 50000,
+        Item = {
+            ID = 3835,
+            Quantity = 5,
+        },
+    },
+    [215408] = {
+        Name = "Waylaid Supplies: Guardian Gloves",
+        Level = 30,
+        Reputation = 850,
+        Money = 50000,
+        Item = {
+            ID = 5966,
+            Quantity = 5,
+        },
+    },
+    [215387] = {
+        Name = "Waylaid Supplies: Heavy Hide",
+        Level = 30,
+        Reputation = 700,
+        Money = 20000,
+        Item = {
+            ID = 4235,
+            Quantity = 5,
+        },
+    },
+    [215420] = {
+        Name = "Waylaid Supplies: Rockscale Cod",
+        Level = 30,
+        Reputation = 700,
+        Money = 20000,
+        Item = {
+            ID = 4594,
+            Quantity = 40,
+        },
+    },
+    [215400] = {
+        Name = "Waylaid Supplies: Solid Grinding Stones",
+        Level = 30,
+        Reputation = 700,
+        Money = 20000,
+        Item = {
+            ID = 7966,
+            Quantity = 5,
+        },
+    },
+    [215391] = {
+        Name = "Waylaid Supplies: Wintersbite",
+        Level = 30,
+        Reputation = 700,
+        Money = 20000,
+        Item = {
+            ID = 3819,
+            Quantity = 8,
+        },
+    },
+    [211819] = {
+        Name = "Waylaid Supplies: Bronze Bars",
+        Level = 25,
+        Reputation = 500,
+        Money = 1500,
+        Item = {
+            ID = 2841,
+            Quantity = 12,
+        },
+    },
+    [211822] = {
+        Name = "Waylaid Supplies: Bruiseweed",
+        Level = 25,
+        Reputation = 500,
+        Money = 1500,
+        Item = {
+            ID = 2453,
+            Quantity = 20,
+        },
+    },
+    [211831] = {
+        Name = "Waylaid Supplies: Dark Leather Cloaks",
+        Level = 25,
+        Reputation = 650,
+        Money = 2000,
+        Item = {
+            ID = 2316,
+            Quantity = 2,
+        },
+    },
+    [211935] = {
+        Name = "Waylaid Supplies: Elixir of Firepower",
+        Level = 25,
+        Reputation = 800,
+        Money = 3000,
+        Item = {
+            ID = 6373,
+            Quantity = 15,
+        },
+    },
+    [211837] = {
+        Name = "Waylaid Supplies: Goblin Deviled Clams",
+        Level = 25,
+        Reputation = 500,
+        Money = 1500,
+        Item = {
+            ID = 5527,
+            Quantity = 8,
+        },
+    },
+    [211833] = {
+        Name = "Waylaid Supplies: Gray Woolen Shirts",
+        Level = 25,
+        Reputation = 650,
+        Money = 2000,
+        Item = {
+            ID = 2587,
+            Quantity = 4,
+        },
+    },
+    [211838] = {
+        Name = "Waylaid Supplies: Heavy Wool Bandages",
+        Level = 25,
+        Reputation = 500,
+        Money = 1500,
+        Item = {
+            ID = 3531,
+            Quantity = 15,
+        },
+    },
+    [211832] = {
+        Name = "Waylaid Supplies: Hillman's Shoulders",
+        Level = 25,
+        Reputation = 800,
+        Money = 3000,
+        Item = {
+            ID = 4251,
+            Quantity = 2,
+        },
+    },
+    [211824] = {
+        Name = "Waylaid Supplies: Lesser Mana Potions",
+        Level = 25,
+        Reputation = 650,
+        Money = 2000,
+        Item = {
+            ID = 3385,
+            Quantity = 20,
+        },
+    },
+    [211821] = {
+        Name = "Waylaid Supplies: Medium Leather",
+        Level = 25,
+        Reputation = 500,
+        Money = 1500,
+        Item = {
+            ID = 2319,
+            Quantity = 12,
+        },
+    },
+    [211828] = {
+        Name = "Waylaid Supplies: Minor Mana Oil",
+        Level = 25,
+        Reputation = 650,
+        Money = 2000,
+        Item = {
+            ID = 20745,
+            Quantity = 2,
+        },
+    },
+    [211830] = {
+        Name = "Waylaid Supplies: Ornate Spyglasses",
+        Level = 25,
+        Reputation = 800,
+        Money = 3000,
+        Item = {
+            ID = 5507,
+            Quantity = 2,
+        },
+    },
+    [211834] = {
+        Name = "Waylaid Supplies: Pearl-clasped Cloaks",
+        Level = 25,
+        Reputation = 800,
+        Money = 3000,
+        Item = {
+            ID = 5542,
+            Quantity = 3,
+        },
+    },
+    [211825] = {
+        Name = "Waylaid Supplies: Rough Bronze Boots",
+        Level = 25,
+        Reputation = 650,
+        Money = 2000,
+        Item = {
+            ID = 6350,
+            Quantity = 3,
+        },
+    },
+    [211827] = {
+        Name = "Waylaid Supplies: Runed Silver Rods",
+        Level = 25,
+        Reputation = 800,
+        Money = 3000,
+        Item = {
+            ID = 6339,
+            Quantity = 1,
+        },
+    },
+    [211820] = {
+        Name = "Waylaid Supplies: Silver Bars",
+        Level = 25,
+        Reputation = 500,
+        Money = 1500,
+        Item = {
+            ID = 2842,
+            Quantity = 6,
+        },
+    },
+    [211826] = {
+        Name = "Waylaid Supplies: Silver Skeleton Keys",
+        Level = 25,
+        Reputation = 800,
+        Money = 3000,
+        Item = {
+            ID = 15869,
+            Quantity = 14,
+        },
+    },
+    [211829] = {
+        Name = "Waylaid Supplies: Small Bronze Bombs",
+        Level = 25,
+        Reputation = 650,
+        Money = 2000,
+        Item = {
+            ID = 4374,
+            Quantity = 12,
+        },
+    },
+    [211836] = {
+        Name = "Waylaid Supplies: Smoked Bear Meat",
+        Level = 25,
+        Reputation = 500,
+        Money = 1500,
+        Item = {
+            ID = 6890,
+            Quantity = 20,
+        },
+    },
+    [211835] = {
+        Name = "Waylaid Supplies: Smoked Sagefish",
+        Level = 25,
+        Reputation = 500,
+        Money = 1500,
+        Item = {
+            ID = 21072,
+            Quantity = 15,
+        },
+    },
+    [211823] = {
+        Name = "Waylaid Supplies: Swiftthistle",
+        Level = 25,
+        Reputation = 500,
+        Money = 1500,
+        Item = {
+            ID = 2452,
+            Quantity = 20,
+        },
+    },
+    [211331] = {
+        Name = "Waylaid Supplies: Brilliant Smallfish",
+        Level = 10,
+        Reputation = 300,
+        Money = 600,
+        Item = {
+            ID = 7751,
+            Quantity = 20,
+        },
+    },
+    [211327] = {
+        Name = "Waylaid Supplies: Brown Linen Pants",
+        Level = 10,
+        Reputation = 450,
+        Money = 1500,
+        Item = {
+            ID = 4343,
+            Quantity = 6,
+        },
+    },
+    [211328] = {
+        Name = "Waylaid Supplies: Brown Linen Robes",
+        Level = 10,
+        Reputation = 450,
+        Money = 1500,
+        Item = {
+            ID = 6238,
+            Quantity = 4,
+        },
+    },
+    [210771] = {
+        Name = "Waylaid Supplies: Copper Bars",
+        Level = 10,
+        Reputation = 300,
+        Money = 600,
+        Item = {
+            ID = 2840,
+            Quantity = 20,
+        },
+    },
+    [211319] = {
+        Name = "Waylaid Supplies: Copper Shortswords",
+        Level = 10,
+        Reputation = 450,
+        Money = 1500,
+        Item = {
+            ID = 2847,
+            Quantity = 6,
+        },
+    },
+    [211326] = {
+        Name = "Waylaid Supplies: Embossed Leather Vests",
+        Level = 10,
+        Reputation = 450,
+        Money = 1500,
+        Item = {
+            ID = 2300,
+            Quantity = 3,
+        },
+    },
+    [211325] = {
+        Name = "Waylaid Supplies: Handstitched Leather Belts",
+        Level = 10,
+        Reputation = 450,
+        Money = 1500,
+        Item = {
+            ID = 4237,
+            Quantity = 5,
+        },
+    },
+    [211934] = {
+        Name = "Waylaid Supplies: Healing Potions",
+        Level = 10,
+        Reputation = 450,
+        Money = 1500,
+        Item = {
+            ID = 929,
+            Quantity = 10,
+        },
+    },
+    [211332] = {
+        Name = "Waylaid Supplies: Heavy Linen Bandages",
+        Level = 10,
+        Reputation = 300,
+        Money = 600,
+        Item = {
+            ID = 2581,
+            Quantity = 10,
+        },
+    },
+    [211329] = {
+        Name = "Waylaid Supplies: Herb Baked Eggs",
+        Level = 10,
+        Reputation = 300,
+        Money = 600,
+        Item = {
+            ID = 6888,
+            Quantity = 20,
+        },
+    },
+    [211321] = {
+        Name = "Waylaid Supplies: Lesser Magic Wands",
+        Level = 10,
+        Reputation = 450,
+        Money = 1500,
+        Item = {
+            ID = 11287,
+            Quantity = 2,
+        },
+    },
+    [211315] = {
+        Name = "Waylaid Supplies: Light Leather",
+        Level = 10,
+        Reputation = 300,
+        Money = 600,
+        Item = {
+            ID = 2318,
+            Quantity = 14,
+        },
+    },
+    [211318] = {
+        Name = "Waylaid Supplies: Minor Healing Potions",
+        Level = 10,
+        Reputation = 450,
+        Money = 1500,
+        Item = {
+            ID = 118,
+            Quantity = 20,
+        },
+    },
+    [211322] = {
+        Name = "Waylaid Supplies: Minor Wizard Oil",
+        Level = 10,
+        Reputation = 450,
+        Money = 1500,
+        Item = {
+            ID = 20744,
+            Quantity = 2,
+        },
+    },
+    [211316] = {
+        Name = "Waylaid Supplies: Peacebloom",
+        Level = 10,
+        Reputation = 300,
+        Money = 600,
+        Item = {
+            ID = 2447,
+            Quantity = 20,
+        },
+    },
+    [211324] = {
+        Name = "Waylaid Supplies: Rough Boomsticks",
+        Level = 10,
+        Reputation = 450,
+        Money = 1500,
+        Item = {
+            ID = 4362,
+            Quantity = 3,
+        },
+    },
+    [211323] = {
+        Name = "Waylaid Supplies: Rough Copper Bombs",
+        Level = 10,
+        Reputation = 450,
+        Money = 1500,
+        Item = {
+            ID = 4360,
+            Quantity = 12,
+        },
+    },
+    [211933] = {
+        Name = "Waylaid Supplies: Rough Stone",
+        Level = 10,
+        Reputation = 300,
+        Money = 600,
+        Item = {
+            ID = 2835,
+            Quantity = 10,
+        },
+    },
+    [211320] = {
+        Name = "Waylaid Supplies: Runed Copper Pants",
+        Level = 10,
+        Reputation = 450,
+        Money = 1500,
+        Item = {
+            ID = 3473,
+            Quantity = 3,
+        },
+    },
+    [211317] = {
+        Name = "Waylaid Supplies: Silverleaf",
+        Level = 10,
+        Reputation = 300,
+        Money = 600,
+        Item = {
+            ID = 765,
+            Quantity = 20,
+        },
+    },
+    [211330] = {
+        Name = "Waylaid Supplies: Spiced Wolf Meat",
+        Level = 10,
+        Reputation = 300,
+        Money = 600,
+        Item = {
+            ID = 2680,
+            Quantity = 20,
+        },
+    },
+}
